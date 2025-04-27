55f96a4a66881a8ad4ba7ddcfa556c192ed0183f
Matthew M. Emma <ibuilder@users.noreply.github.com>
2025-04-27 18:36:35 -0400

    dbfix

--- config/database.php
+++ config/database.php
@@ -1,20 +1,54 @@
 <?php
 
 return [
-    'default' => env('DB_CONNECTION', 'supabase'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     'connections' => [
-        'supabase' => [
-            'driver' => 'pgsql',
-            'host' => env('SUPABASE_DB_HOST', 'your_supabase_host'),
-            'port' => env('SUPABASE_DB_PORT', '5432'),
-            'database' => env('SUPABASE_DB_NAME', 'your_database_name'),
-            'username' => env('SUPABASE_DB_USER', 'your_username'),
-            'password' => env('SUPABASE_DB_PASSWORD', 'your_password'),
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'database' => env('DB_DATABASE', ':memory:'),
             'prefix' => '',
-            'schema' => 'public',
+        ],
+
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+        ],
+
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
+        ],
+
+        'sqlsrv' => [
+            'driver' => 'sqlsrv',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '1433'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
         ],
     ],
 
